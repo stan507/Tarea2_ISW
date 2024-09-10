88942ea8af7cf5a394261dbfdf4e11a0a1dfcbb6
stan507 <juan.henriquez2101@alumnos.ubiobio.cl>
2024-09-10 19:21:30 -0300

    Tarea sin terminar

---
+++ Primer_codigo.js
@@ -0,0 +1,42 @@
+/*
+Variables y tipos de datos
+*/ 
+
+
+const nombre = "Juan";
+const edad = 21;
+const puedoConducir = true;
+
+console.log(nombre);
+console.log(typeof nombre);
+console.log(edad);
+console.log(typeof edad);
+console.log(puedoConducir);
+console.log(typeof puedoConducir);
+
+/*
+Operadores aritméticos
+*/
+
+const numero1 = 20;
+const numero2 = 40;
+let numero3 = null;
+
+/*Suma*/ 
+numero3 = numero1 + numero2;
+console.log(numero3);
+/*Resta*/
+numero3 = numero1 - numero2;
+console.log(numero3);
+/*Multiplicación*/
+numero3 = numero1 * numero2;
+console.log(numero3);
+/*División*/
+numero3 = numero1 / numero2;
+console.log(numero3);
+/*Módulo*/
+numero3 = numero1 % numero2;
+console.log(numero3);
+
+
+
